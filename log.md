b0acc094cadb7788c7c292cda38961c06db884b8
rashmi-fit <rashmim282@gmail.com>
2022-04-17 22:43:03 +0100

    [RM]day0 of python coding
    I tried to solve roman to integer conversion and you can find more information in log file

--- log.md
+++ log.md
@@ -1,7 +1,9 @@
 Log of Round 1 of #100DaysOfCode - 
-Day 1: April 17. Sunday
+Day 0: April 17. Sunday
 Today's Progress: Spent time fixing CSS and figuring out how canvas works.
 
-Thoughts: Feels good to be back at coding.
+Thoughts: Feels good and excited to Code.I struggled a lot and googled how it can be done. I solved it my own. It took me almost more than 1 hour to solve this.
 
-Link to work: Pomodoro Clock App
+Link to work: 
+
+Day 1: April 18. Sunday
---
+++ romanToInteger.py
@@ -0,0 +1,25 @@
+''' https://leetcode.com/problems/roman-to-integer/ '''
+'''back to front
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+'''
+def romanToInteger(string):
+
+    my_dict={ "I"   :  1,'V': 5,'X': 10,'L' : 50,'C' : 100,'D' : 500,'M' : 1000 }
+    print(my_dict)
+    output=0
+    N=len(string)-1
+    i=N-1
+    while i>=0:
+        if my_dict[string[i]]>my_dict[string[i+1]] and i<N-1:
+            output=output - my_dict[string[i]]
+            # print("output")
+        else:
+            output=output+ my_dict[string[i]]
+            i-=1
+    return output
+ 
+print(romanToInteger('III'))
