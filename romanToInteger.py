08340544a2014f12b73a298db0dd299836c0c34e
rashmi-fit <rashmim282@gmail.com>
2022-04-17 23:57:51 +0100

    [RM]using for loop day0

--- romanToInteger.py
+++ romanToInteger.py
@@ -1,26 +1,26 @@
-''' https://leetcode.com/problems/roman-to-integer/ '''
-'''back to front
-Constraints:
+# ''' https://leetcode.com/problems/roman-to-integer/ 
+# back to front
+# Constraints:
 
-1 <= s.length <= 15
-s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
-It is guaranteed that s is a valid roman numeral in the range [1, 3999].
-'''
+# 1 <= s.length <= 15
+# s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+# It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+# '''
+
+
 def romanToInteger(string):
 
-    my_dict={ "I"   :  1,'V': 5,'X': 10,'L' : 50,'C' : 100,'D' : 500,'M' : 1000 }
+    my_dict = {"I":  1, 'V': 5, 'X': 10,
+               'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     print(my_dict)
-    output=0
-    N=len(string)
-    i=N-1
-    while i>=0:
-        # check the previous is more than current one
-        if i < N-1 & my_dict[string[i]] < my_dict[string[i+1]] : 
-            output= output - my_dict[string[i]]
-            # print("output")
+    output = 0
+    # i=len(string)-1
+    for i in range(len(string)):
+        if i+1 != len(string) and my_dict[string[i]] < my_dict[string[i+1]]:
+            output = output - my_dict[string[i]]
         else:
-            output=output+ my_dict[string[i]]
-            i-=1
+            output = output + my_dict[string[i]]
     return output
- 
+
+
 print(romanToInteger('III'))
