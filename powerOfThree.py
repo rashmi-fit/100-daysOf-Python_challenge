ee31a955370066ee033b3c257c5c84ab264f2207
rashmi-fit <rashmim282@gmail.com>
2022-05-11 22:26:57 +0100

    Update powerOfThree.py

--- powerOfThree.py
+++ powerOfThree.py
@@ -15,11 +15,21 @@ An integer n is a power of three, if there exists an integer x such that n == 3x
 #         print(f"false {num}")
 #         break
 
-num=int(input("enter a number: "))
-if num==0:
-    print(f"false {num}")
+# num=int(input("enter a number: "))
+# if num==0:
+#     print(f"false {num}")
 
-while num!=1:
+# while num!=1:
+#     if num%3!=0:
+#         print(f"false {num}")
+#     num=num//3
+# print(f"true {num}")
+
+num=int(input("enter a number: "))
+# if num==0:
+#     print(f"false {num}")
+
+while num>0:
     if num%3!=0:
         print(f"false {num}")
     num=num//3
