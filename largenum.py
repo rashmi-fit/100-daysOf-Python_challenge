6ad483d4134852c1d96b6b096bb0444b05f6a6b2
rashmi-fit <rashmim282@gmail.com>
2022-05-16 00:16:07 +0100

    [RM]DAY 27 largest num

---
+++ largenum.py
@@ -0,0 +1,13 @@
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
+
